12b3cf275035d4dd88fd93222e03968a60985263
Steve Mostovoy <stevemostovoysm@gmail.com>
2018-12-31 21:02:00 -0500

    Swap uws for @clusterws/cws.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fp = require('fastify-plugin')
 module.exports = fp((fastify, opts, next) => {
   const lib = opts.library || 'ws'
 
-  if (lib !== 'ws' && lib !== 'uws') return next(new Error('Invalid "library" option'))
+  if (lib !== 'ws' && lib !== '@clusterws/cws') return next(new Error('Invalid "library" option'))
 
   const WebSocketServer = require(lib).Server
   const wss = new WebSocketServer({
